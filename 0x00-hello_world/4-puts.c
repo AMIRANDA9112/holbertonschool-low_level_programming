2f4a187b9c66d76429f36c1d6c11997082e02da6
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-05 23:55:41 +0000

    the space and betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual puzzle
- *
- * Return: Always 0 (success)
- */
+* main - Prints "Programming is like building a multilingual puzzle
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
