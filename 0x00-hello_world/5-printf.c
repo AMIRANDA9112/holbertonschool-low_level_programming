802018616db8afe583c0da4dff41c9f8ec955085
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-06 01:16:51 +0000

    printf need newline

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Prints "Programming is like building a multilingual puzzle
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
