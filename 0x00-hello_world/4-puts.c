e078fb63b9ac8dd3e5e8cf1fb41e1f48d954a5da
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-06 00:52:50 +0000

    sintaxis everytime

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /*
 * main - Prints "Programming is like building a multilingual puzzle
 *
@@ -9,6 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
 }
