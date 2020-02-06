bdf34de14e8d3dbaf47366d134b69ad4791d6608
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-06 00:36:06 +0000

    puts dont need newline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/**
+
+/*
 * main - Prints "Programming is like building a multilingual puzzle
 *
 * Return: Always 0 (success)
 */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
