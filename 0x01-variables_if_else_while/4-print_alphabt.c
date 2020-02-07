ef89d278968193064361de6a949bf390196c5234
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-07 04:18:29 +0000

    if hoy condicional

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls = 'a' ; ls <= 'z' ; ch++)
+{
+	if (ls != 'e' ; ls != 'q')
+	putchar (ls);
+}
+	putchar('\n');
+	return (0);
+}
