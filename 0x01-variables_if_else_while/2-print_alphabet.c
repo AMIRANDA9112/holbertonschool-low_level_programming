2d5bc8e36919a32e36defcec93167d71938356dd
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-07 03:45:27 +0000

    remeber for

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char m;
+
+	for (m = 'a' ; m <= 'z' ; ch++)
+{
+	putchar (m);
+}
+	putchar('\n');
+	return (0);
+}
