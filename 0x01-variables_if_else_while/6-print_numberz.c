1fb36b9f27258bed859f7ccf8a2b0ba65177b179
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-07 04:28:07 +0000

    again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (succes)
+ */
+
+	int main(void)
+
+{
+	int ls;
+
+	for (ls = 0; ls < 10; ls++)
+	putchar(48 + ls);
+	putchar(10);
+	return(0);
+}
