72a931123c10083ba3fbbc86249945bcc8ae3164
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-07 04:55:49 +0000

    again

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -12,18 +12,18 @@ int main(void)
 	int s2 = 0;
 
 	for (s1 = 0; s1 <= 9; s1++)
-	{
-		for (s2 = s1 + 1; s2 <= 9; s2++)
-		{
-			putchar(s1 + 48);
-			putchar(s2 + 48);
-			if (s1 < 8)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+{
+	for (s2 = s1 + 1; s2 <= 9; s2++)
+{
+	putchar(s1 + 48);
+	putchar(s2 + 48);
+	if (s1 < 8)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
 	putchar('\n');
 	return (0);
 }
