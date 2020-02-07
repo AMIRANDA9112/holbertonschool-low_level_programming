d41a285d4f42997e4bb94da74cebb37724a36889
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-07 04:22:32 +0000

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls = 48 ; ls <= 57 ; ls++)
+
+		putchar(ls);
+
+
+	putchar('\n');
+
+	return (0);
+}
