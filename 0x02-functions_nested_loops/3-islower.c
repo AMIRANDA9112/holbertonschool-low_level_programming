1d56036c5382edb823e90a4e980e3d81792916fb
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-11 20:51:56 +0000

    verify lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	if (_islower(i))
+{
+	_putchar ('1')
+}
+	else 
+{
+	_putchar ('0')
+}
+	return	(0);
+}
