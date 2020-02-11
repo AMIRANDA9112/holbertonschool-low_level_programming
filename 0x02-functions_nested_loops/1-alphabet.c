bbba574219f7990505c8e8db1fcd0bc288ac072d
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-11 15:57:25 +0000

    my first main function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - start of the program
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+
+{
+	char i = 'a';
+
+	while (i <= 'z')
+{
+	_putchar(i);
+	i++;
+}
+	if (i > 'z')
+
+{
+	_putchar('\n');
+}
+}
