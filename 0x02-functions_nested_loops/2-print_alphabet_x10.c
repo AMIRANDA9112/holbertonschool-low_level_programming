d433b6354bcf06b09b81c966168f41fa48dfb98e
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-11 19:12:44 +0000

    ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet - start of the program
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+{
+	while (i <= 'z')
+{
+	_putchar(i);
+	i++;
+}
+	if (i > 'z')
+{
+	_putchar('\n');
+}
+}
+}
