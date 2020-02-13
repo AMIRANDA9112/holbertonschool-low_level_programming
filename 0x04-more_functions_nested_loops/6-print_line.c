f246fb13796fb0a94bf1faf8b2c779538d807fa0
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-13 22:58:31 +0000

    again

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_line - print line.
+ *@n: vaible.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int m;
+
+		for (m = 0; m <= n; m++)
+
+		{
+			_putchar(95);
+
+			_putchar('\n');
+		}
+
+	}
+	else
+		{
+			_putcar('\n');
+		}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,5 +6,6 @@ int _isupper(int c);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 int mul(int a, int b);
 #endif
