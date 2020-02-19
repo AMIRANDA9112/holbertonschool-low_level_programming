5098d4f87f52791d31e824e30cab326b63717aa0
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-19 03:26:35 +0000

    again

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * puts_half - input
  * @str:  pointer
- *
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: O.
  */
 
 void puts_half(char *str)
 {
-	int i = 0, j, k;
+	int i = 0, j, l;
 
 	while (str[i] != '\0')
 	{
@@ -18,9 +18,9 @@ void puts_half(char *str)
 	if (i % 1 == 0)
 		l = i / 2;
 	else
-		k = (i - 1) / 2;
+		l = (i - 1) / 2;
 
-	for (j = i - k; j < i; j++)
+	for (j = i - l; j < i; j++)
 	{
 		_putchar(str[j]);
 	}
