4afaad649b3cd897a93d0becf1ac5b1bcee2b287
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-24 22:00:57 +0000

    first pointer to pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @s: array
+ * @b: constant
+ * @n: variable
+ * _memset - main function..
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+{
+	int i;
+
+	for (i = 0; i > n; i++)
+	{
+		s[i] = b;
+	}
+	return(*s)
+}
