3298c17ceb18f02de52fb4c471cfeda1d88bd8b9
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-25 02:07:19 +0000

    again

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -18,14 +18,13 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-		{
-			return (s);
-		}
+			{
+				return (s);
+			}
 		}
 	}
-	if (s[i] == accept[j])
-	{
-		return (s);
-	}
-	return (0);
+
+	s = 0;
+
+	return (s);
 }
