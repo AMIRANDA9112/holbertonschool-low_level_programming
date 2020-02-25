bb85a5414bd428980f1edeac8cf2f7dce651c329
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-25 01:07:10 +0000

    again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,23 @@
  *
  * Return: char
  */
+char *_strchr(char *s, char c)
+{
+	int i;
 
+	for (i = 0; s[i] != '\0' ; s++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	return (0);
+}
 char *_strchr(char *s, char c)
 {
 	int i;
@@ -18,6 +34,9 @@ char *_strchr(char *s, char c)
 			return (s);
 		}
 	}
-
-	return (NULL);
+	if (s[i] == c)
+	{
+		return (s);
+	}
+	return (0);
 }
