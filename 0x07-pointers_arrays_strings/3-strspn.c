4f96ac10f582186f91f22895e012961ed2ba4fc3
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-25 01:15:03 +0000

    again

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *_strspn - locates a character in a string
+ * @s: string base
+ * @accept: string to compare
+ *
+ * Return: i count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		break;
+	}
+
+return (i);
+}
