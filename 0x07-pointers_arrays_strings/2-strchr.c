e703417cfaaa1359d2cbf2b9bb0392e6ab46d122
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-25 00:30:01 +0000

    reinicial

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string
+ * @c: pointer to the source of dat to be copied
+ *
+ * Return: s ( char o NULL)
+ */
+char *_strchr(char *s, char c)
+{
+	for (s = 0; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+		return (s);
+	s = '\0';
+
+	return (s);
+}
