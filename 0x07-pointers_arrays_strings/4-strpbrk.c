2ea69d598bc59d6ad00141644b91d4b0745927ff
AMIRANDA9112 <andreselfm@gmail.com>
2020-02-25 01:36:45 +0000

    one time

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - main function
+ * @s: string base
+ * @accept: string to compare
+ *
+ * Return:  char
+ */
+char *_strpbrk(char *s, char *accept)
+
+{
+	int j;
+	int i;
+
+	for (i = 0; s[i] != '\0' ; s++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+		{
+			return (s);
+		}
+		}
+	}
+	if (s[i] == accept[j])
+	{
+		return (s);
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
