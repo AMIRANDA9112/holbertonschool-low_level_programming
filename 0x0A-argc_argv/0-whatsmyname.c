394bc13fd91d7347f8cecf5556e1b2b3e588e654
AMIRANDA9112} <andreselfm@gmail.com>
2020-03-01 22:22:31 -0500

    again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
  * @argv: array tha contains the arguments
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)),
+	char *argv[] __attribute__((unused)))
 {
 	printf("%s\n", *argv);
 	return  (0);
