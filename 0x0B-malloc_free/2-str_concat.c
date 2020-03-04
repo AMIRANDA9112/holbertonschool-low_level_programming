1a30dbe8eacc3e8483f687fa9f30863fcfe0ec28
AMIRANDA9112} <andreselfm@gmail.com>
2020-03-03 22:58:20 -0500

    correct

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,39 +15,51 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	i = j = k = l = 0;
+
 	if (s1 == NULL)
 	{
 		s1 = malloc(1 * (sizeof(char)));
 		s1[0] = '\0';
 	}
+
 	if (s2 == NULL)
 	{
 		s2 = malloc(1 * (sizeof(char)));
 		s2[0] = '\0';
 	}
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
+
+		while (s1[i] != '\0')
+		{
+			i++;
+		}
+
+		while (s2[j] != '\0')
+		{
+			j++;
+		}
+
 	size = j + i;
 	ptr = malloc(size * (sizeof(char)));
+
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
+
 	l = 0;
+
 	while (k < i)
 	{
 		ptr[k] = s1[k];
 			k++;
 	}
+
 	while (k <= size)
 	{
 		ptr[k] = s2[l];
 		k++;
 		l++;
 	}
+
 	return (ptr);
 }
