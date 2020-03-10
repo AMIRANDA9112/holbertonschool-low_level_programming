7d1385391778df0406d5e3cac54a53f029cc1499
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-10 01:14:48 +0000

    again

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -28,5 +28,5 @@ void print_dog(struct dog *d)
 		printf("Owner: %s\n", d->owner);
 		}
 	}
-return;
+	return;
 }
--- 0x0E-structures_typedef/holberton.h
+++ 0x0E-structures_typedef/holberton.h
@@ -1,6 +1,6 @@
 # ifndef HOLBERTON_H
 # define  HOLBERTON_H
-
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 # endif
