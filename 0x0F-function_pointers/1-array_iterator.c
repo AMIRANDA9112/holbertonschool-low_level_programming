65a428b3c0725b068a9d40b7b4ecb66bef06550b
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-11 21:19:13 +0000

    betty resolve

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - prints a name
- * @name: name of the person
- * @f: the function pointer to use
+ * array_iterator - prints an array
+ * @array: Integer array
+ * @size: number of elements of the array
+ * @action: function pointer to use
  * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
- 	size_t count;
+	size_t count;
 	int pos;
 
-	if ((array) && (size) && (action))
-
-{
+if (array)
+	if (size)
+		if (action)
+		{
 			for (count = 0; count < size ; count++)
 				{
-				pos = array [count];
+				pos = array[count];
 				action(pos);
 				}
 		}
