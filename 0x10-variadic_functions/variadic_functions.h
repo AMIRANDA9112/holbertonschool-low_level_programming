66510d50df8ab84e63b7d977de9fbe39f554f352
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-13 20:51:38 +0000

    again

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,7 @@
 #include <stdarg.h>
 # ifndef VARIADIC_FUNCTIONS_H
 # define   VARIADIC_FUNCTIONS_H
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 # endif
