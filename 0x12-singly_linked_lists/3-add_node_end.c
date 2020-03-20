bb063ce86278c64299ad3ad4d2931ff44f3b7c71
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-19 22:40:53 -0500

    again

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,29 +5,35 @@
  * @str: string to be saved in new node, must be duplicated
  * Return: Address of new element or NULL if failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new = malloc(sizeof(list_t));
+	list_t *transit=  *head;
 
 	int i = 0;
 
 	while (str[i])
-	{
 		i++;
-	}
 
 	if (str == NULL)
-		return (0);
-
-	new = malloc(sizeof(list_t));
+		return (NULL);
 
 	if (new == NULL)
-		return (0);
+		return(NULL);
 
 	new->str = strdup(str);
 	new->len = i;
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
 
-	return (new);
+	if (*head == NULL)
+	{
+		*head = new;
+		return(*head);
+	}
+
+	if(transit->next != NULL)
+		transit = transit->next;
+
+	transit->next = new;
+	return(*head);
 }
