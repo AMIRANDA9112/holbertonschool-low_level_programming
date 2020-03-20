fb3229dc9ff9e84f887f15a6ea8e1b4389301acd
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-19 20:31:55 -0500

    header no actualizado

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,30 @@
-# include <stdio.h>
-# ifndef VARIADIC_FUNCTIONS_H
-# define   VARIADIC_FUNCTIONS_H
-int _putchar(char c);
-size_t print_list(const list_t *h);
-# endif
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Struct */
+
+/**
+ * struct list - linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list
+ */
+typedef struct list
+{
+	char *str;
+	unsigned int len;
+	struct list *next;
+} list_t;
+
+
+/* Prototype */
+
+size_t print_list(const list_t *h);
+
+#endif
