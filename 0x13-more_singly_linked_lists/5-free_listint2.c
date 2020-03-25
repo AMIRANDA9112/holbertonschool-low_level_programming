446e321f7451b62edea44f9a5346124bd2965f4a
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-24 21:59:27 -0500

    again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,17 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *fr;
 
-	if (*head)
-	{
-
 	while (*head)
 	{
 		fr = (*head)->next;
 		free(*head);
 		*head = fr;
 	}
-	}
-	else
+
 	*head = NULL;
 }
 
