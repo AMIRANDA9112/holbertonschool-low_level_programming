0dc24fd7578273993976b960bd4283c99aaedd97
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-25 22:29:46 -0500

    betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 
 		if (loc == NULL)
-			return NULL;
+			return (NULL);
 	}
 
 	new = malloc(sizeof(listint_t));
