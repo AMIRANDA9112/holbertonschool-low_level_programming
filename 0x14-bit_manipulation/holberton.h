885536c56294288993742867f652d27d3c7ac8c9
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-28 21:29:08 -0500

    again

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,15 +1,13 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
+#ifndef holberton_h
+#define holberton_h
 #include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
+#include <math.h>
+#include <string.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
 #endif
