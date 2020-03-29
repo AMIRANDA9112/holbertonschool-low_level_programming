d8cd3ce930971fa5f8c7c05c021602d66ab75fd6
AMIRANDA9112 <andreselfm@gmail.com>
2020-03-28 21:23:12 -0500

    task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * get_bit - a function that get a bit
+ * @n: an unsigned long integer
+ * @index: this is a index
+ * Return: an integer
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int long r;
+
+	if (index > 64)
+		return (-1);
+
+	r = (((n >> index) & 1));
+
+return (r);
+}
