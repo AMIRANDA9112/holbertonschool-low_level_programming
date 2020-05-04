ed2033db3fbe85d688a903fcf53906bb1a9d5c40
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-03 20:14:59 -0500

    lines

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,16 @@
 #include "lists.h"
-
 /**
  * print_dlistint - print all elements to a double linked list
  * description - follow de flow of a list to print all positions.
  *@h : pointer of list to print
- * return -  size of list to print
+ * Return :  size of list to print
 **/
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 	const dlistint_t *lector;
-	lector = h;
 
+	lector = h;
 	while (lector)
 	{
 		printf("%d\n", lector->n);
