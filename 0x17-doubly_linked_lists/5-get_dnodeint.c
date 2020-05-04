280a2be33fb954e6b77f20b6466f227591d78482
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-03 23:10:53 -0500

    index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get the content of index
+ * @head: pointer to first element
+ * @index: position on list
+ * Return: elemen of position index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int leng = index;
+	unsigned int i = 0;
+	unsigned int p = dlistint_len(head);
+	dlistint_t *runner;
+
+	if (leng > p)
+		return (NULL);
+
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+
+
+	runner = head;
+
+	return (runner);
+}
+
+
