8adf3182befbfea8cf0c858f0db9bfcecac906c7
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-03 20:23:08 -0500

    length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - count the number of nodes in double single list
+ * *@h : pointer of list to analize
+ * Return:  size of list to print
+**/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	const dlistint_t *lector;
+
+	lector = h;
+	while (lector)
+	{
+		i++;
+		lector = lector->next;
+	}
+	return (i);
+}
