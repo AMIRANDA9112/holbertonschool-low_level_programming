654ff31290f266220a3f0af5caf182895f3956f0
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-03 20:13:18 -0500

    comment issue resolve

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
  * description - follow de flow of a list to print all positions.
  *@h : pointer of list to print
  * return -  size of list to print
-/**
+**/
 
 size_t print_dlistint(const dlistint_t *h)
 {
