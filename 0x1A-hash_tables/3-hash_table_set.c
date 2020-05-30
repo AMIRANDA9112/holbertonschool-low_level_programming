e1c76a7494954fb87e2d37c7299dd93f2ff2e01d
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-29 22:45:27 -0500

    set again

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,49 +4,43 @@
  * @ht: pointer to hash table
  * @key: string, input data
  * @value: The value of key
- * Description: Returns the index at which the key/value pair should be stored
- *  in the array of the hash table
+ * Description: Add a new node.
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    node_t *newNode = NULL, *tmp = NULL;
-    unsigned long int index = 0;
+	hash_node_t *key_node = NULL, *tmp = NULL;
+	unsigned long int index = 0;
 
-    if (ht == NULL || key == NULL || value == NULL)
-        return (0);
+	if (ht)
+	{
+		if (key && strlen(key) > 0)
+			index = key_index((unsigned char *)key, ht->size);
+		else
+			return (0);
 
-    index = key_index((unsigned char *)key, ht->size);
+		tmp = ht->array[index];
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
 
-    for (tmp = ht->array[index]; tmp != NULL; tmp = tmp->next)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            if (tmp->value == NULL)
-                return (0);
-            return (1);
-        }
-    }
-    newNode = malloc(sizeof(node_t));
-    if (newNode == NULL)
-        return (0);
+		key_node = malloc(sizeof(hash_node_t));
+		if (!key_node)
+			return (0);
+		key_node->key = strdup(key);
+		key_node->value = strdup(value);
+		key_node->next = ht->array[index];
+		ht->array[index] = key_node;
 
-    newNode->key = strdup(key);
-    if (newNode->key == NULL)
-    {
-        free(newNode);
-        return (0);
-    }
-    newNode->value = strdup(value);
-    if (newNode->value == NULL)
-    {
-        free(newNode->key);
-        free(newNode);
-        return (0);
-    }
-    newNode->next = ht->array[index];
-    ht->array[index] = newNode;
-    return (1);
+		return (1);
+	}
+	return (0);
+
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -11,22 +11,22 @@
  * @value: The value of key
  * @next: A pointer to the next node
  */
-typedef struct node_s
+typedef struct hash_node_s
 {
 	char *key;
 	char *value;
-	struct node_s *next;
-} node_t;
+	struct hash_node_s *next;
+} hash_node_t;
 /**
  * struct table_s - Hash table
  *
  * @size: The size of array
  * @array: array of size @size
  */
-typedef struct table_s
+typedef struct hash_table_s
 {
 	unsigned long int size;
-	node_t **array;
+    hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
