b13776d40ecf30adfffb12eb2d659ddf4c0a2a35
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-29 19:37:16 -0500

    my fisrt hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,23 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//CREATE HASH TABLE//
-
-//NODE//
+/**
+ * struct node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct node_s
 {
-    char *key;
-    char *value;
-    struct node_s *next;
+	char *key;
+	char *value;
+	struct node_s *next;
 } node_t;
-
-//TABLE
 
 typedef struct table_s
 {
-    unsigned long int size;
-    node_t **array;
+	unsigned long int size;
+	node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-#endif //HASH_TABLES_H
+#endif
